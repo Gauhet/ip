1bb5d9eb3a019c5f2f8f38e654dee95e1e97aa69
Gauhet <gauhetpoddar@gmail.com>
2026-08-21 12:51:04 +0800

    Add greeting and exit messages for AlredTheButtler

---
+++ src/main/java/AlfredTheButtler.java
@@ -0,0 +1,12 @@
+public class AlfredTheButtler {
+    public static void main(String[] args) {
+	    String line = "------------------------------------------------";
+	    String name = "AlfredTheButtler";
+	    System.out.println(line);
+	    System.out.println("Hello! I'm " + name);
+	    System.out.println("What can I do for you?");
+	    System.out.println(line);
+	    System.out.println("Bye. Hope to see you again soon!");
+	    System.out.println(line);
+    }
+}
